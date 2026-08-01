bf41d9ee5c3fabb2d66744b331e32d03a6d81b20
Finn Dohrmann <kontakt@finndohrmann.de>
2026-08-01 18:22:58 +0200

    Begin writing docs

--- api-service/src/main/java/dev/minceraft/sonus/api/service/ISonusServiceApi.java
+++ api-service/src/main/java/dev/minceraft/sonus/api/service/ISonusServiceApi.java
@@ -8,6 +8,9 @@ import net.kyori.adventure.util.Services;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
+/**
+ * The main API class for the Sonus Service
+ */
 public interface ISonusServiceApi {
 
     ISonusPlayerManager getPlayerManager();
@@ -16,10 +19,34 @@ public interface ISonusServiceApi {
 
     ISonusEventManager getEventManager();
 
+    /**
+     * Creates the sonus audio wrapper with pcm data
+     * <p>
+     * You can also convert it into opus with {@link ISonusAudio#getOpus()}
+     *
+     * @param sequence the sequence number of the audio part
+     * @param pcm the raw pcm data
+     * @return an instance of ISonusAudio holding the information
+     */
     ISonusAudio audioFromPcm(long sequence, short[] pcm);
 
+    /**
+     * Create the sonus audio wrapper with opus data
+     * <p>
+     * You can also convert it into pcm with {@link ISonusAudio#getPcm()}
+     *
+     * @param sequence the sequence number of the audio part
+     * @param opus the raw opus data
+     * @return an instance of ISonusAudio holding the information
+     */
     ISonusAudio audioFromOpus(long sequence, byte[] opus);
 
+    /**
+     * Utility method to get the instance of the SonusServiceApi
+     * <strong>WARNING: If Sonus is not properly enabled before using this method, it will throw an exception</strong>
+     * @return the instance of the SonusServiceApi
+     * @throws IllegalStateException if SonusService is not enabled before calling this method
+     */
     static ISonusServiceApi getInstance() {
         if (InstanceHolder.API == null) {
             try {
--- api-service/src/main/java/dev/minceraft/sonus/api/service/participant/builtin/ISonusServicePlayer.java
+++ api-service/src/main/java/dev/minceraft/sonus/api/service/participant/builtin/ISonusServicePlayer.java
@@ -13,33 +13,94 @@ import java.util.Locale;
 @NullMarked
 public interface ISonusServicePlayer extends ISonusSource, ISonusListener {
 
+    /**
+     * Returns the display name of the player, with a viewer context.
+     * The context can be used to respect nick systems. If this isn't need use {@link #getName()} instead.
+     *
+     * @param viewer the viewer of the player
+     * @return the displayname
+     */
     String getName(@Nullable ISonusServicePlayer viewer);
 
+    /**
+     * Returns the display name of the player
+     *
+     * @return the display name
+     */
     default String getName() {
         return this.getName(null);
     }
 
+    /**
+     * Sonus periodically checks if the player is still connected to the server. This method returns the last time Sonus received a keep alive from the player.
+     *
+     * @return the lastest keep alive timestamp in ms
+     */
     long getLastKeepAlive();
 
+    /**
+     * The Sonus backend agent will transmit the players team. Used for special rooms or context based audio routing.
+     *
+     * @return the players team
+     */
     @Nullable
     String getTeam();
 
+    /**
+     * Checks if a player can join the specified room. Password and permission check
+     *
+     * @param room     the room to check access for
+     * @param password the password for the room
+     * @return true if the player can access the room, false otherwise
+     */
     boolean canAccessRoom(ISonusRoom room, @Nullable String password);
 
+    /**
+     * Joins the player into the specified room. It ignores all permissions and forces the join.
+     *
+     * @param room the target room to join
+     */
     void joinRoom(ISonusRoom room);
 
+    /**
+     * Removes the player from the specified room.
+     *
+     * @param room the target room to leave
+     */
     void leaveRoom(ISonusRoom room);
 
+    /**
+     * Returns the server room of the player. The server room is a special room that is used to route audio to all players on the same server.
+     *
+     * @return the server room
+     */
     @Nullable
     ISonusRoom getServerRoom();
 
+    /**
+     * Sets the server room.
+     *
+     * @param room the target server room
+     */
     void setServerRoom(@Nullable ISonusRoom room);
 
+    /**
+     * Returns the primary room of the player. For example the classic group rooms with multiple players.
+     *
+     * @return the primary room
+     */
     @Nullable
     ISonusRoom getPrimaryRoom();
 
+    /**
+     * Sets the primary room of the player. For example the classic group rooms with multiple players.
+     * <p>
+     * Use null to leave
+     *
+     * @param room target room
+     */
     void setPrimaryRoom(@Nullable ISonusRoom room);
-
+    
     boolean isMuted();
 
     default void setMuted(boolean muted) {
